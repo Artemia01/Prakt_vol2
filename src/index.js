225d5cc79f01fdea91671ec5fe594f50241a133a
Artemia01 <artem200330@gmail.com>
2023-01-12 21:03:18 +0200

    Day 1

--- src/index.js
+++ src/index.js
@@ -5,21 +5,21 @@ import VueAxios from 'vue-axios'
 var students = [{
     "id" : 1,
     "pib" : "Andrii Kurkov",
-    "zdav" : true,
+    "isDonePr" : true,
     "group" : "RPZ 19 2/9",
     "src": "https://orig00.deviantart.net/ee08/f/2009/073/e/d/free_red_panda_icon_100x100_by_supertuffpinkpuff.png"
 },
 {
     "id" : 2,
     "pib" : "Serhii Plohij",
-    "zdav" : false,
+    "isDonePr" : false,
     "group" : "RPZ 19 1/9",
     "src": "https://cdn-learn.adafruit.com/assets/assets/000/012/878/thumb100/led_strips_doge.bmp?1386611464"
 },
 {
     "id" : 3,
     "pib" : "Yurko Izdryk",
-    "zdav" : true,
+    "isDonePr" : true,
     "group" : "RPZ 19 2/9",
     "src": "https://vignette.wikia.nocookie.net/adventuretime/images/9/99/AT_Icons_100x100_Jake.jpg/revision/latest?cb=20120919222926&path-prefix=ar"
 }
@@ -30,21 +30,21 @@ createApp({
        return {
            students: [],
            search:'',
-           student: {"pib": "", zdav: false, group: ""}
+           student: {"name": "", isDonePr: false, group: ""}
        }
     },
     mounted: function  () {
-        axios.get("http://46.101.212.195:3000/students").then((response) =>{
+        axios.get("http://34.82.81.113:3000/students").then((response) =>{
             console.log(response.data);
             this.students = response.data;
         })
     },
     methods: {
-       deleteStudent(studId) {
-           this.students = this.students.filter(elem => {
-               return elem.id != studId;
-           });
-       },
+        deleteStudent(studId) {
+            const index = this.students.findIndex((elem) => elem.id === studId);
+            this.students.splice(index, 1);
+        },
+
        addStudent() {
            this.student.id = this.students.length+1;
            this.students.push(this.student);
