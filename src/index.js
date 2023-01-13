8be22d8355d3ac0a96300e682f0c8804eab2cb7c
Artemia01 <artem200330@gmail.com>
2023-01-13 22:00:51 +0200

    Day 1 addTask

--- src/index.js
+++ src/index.js
@@ -2,28 +2,7 @@ import { createApp } from 'vue'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
 
-var students = [{
-    "id" : 1,
-    "pib" : "Andrii Kurkov",
-    "isDonePr" : true,
-    "group" : "RPZ 19 2/9",
-    "src": "https://orig00.deviantart.net/ee08/f/2009/073/e/d/free_red_panda_icon_100x100_by_supertuffpinkpuff.png"
-},
-{
-    "id" : 2,
-    "pib" : "Serhii Plohij",
-    "isDonePr" : false,
-    "group" : "RPZ 19 1/9",
-    "src": "https://cdn-learn.adafruit.com/assets/assets/000/012/878/thumb100/led_strips_doge.bmp?1386611464"
-},
-{
-    "id" : 3,
-    "pib" : "Yurko Izdryk",
-    "isDonePr" : true,
-    "group" : "RPZ 19 2/9",
-    "src": "https://vignette.wikia.nocookie.net/adventuretime/images/9/99/AT_Icons_100x100_Jake.jpg/revision/latest?cb=20120919222926&path-prefix=ar"
-}
-]
+
 
 const app = createApp({
     data() {
@@ -31,12 +10,33 @@ const app = createApp({
         students: [],
         search: "",
         student: { name: "", zdav: false, group: "" },
+        
+        convertorData: [],
+        convAmount: 1,
+        convResult: 1,
+
+        convHH: 1,
+        convDD: 1,
+
+        convHD: 1,
+        convDH: 1,
+
+        convFrom: 'D',
+        convTo: 'H',
+
       };
     },
     mounted: function () {
       axios.get("http://34.82.81.113:3000/students").then((response) => {
         this.students = response.data;
       });
+      axios.get("https://api.monobank.ua/bank/currency").then((res) =>{
+        console.log(res.data); 
+        this.convertorData = res.data;
+
+        this.convDH = this.convertorData[0].rateBuy;
+        this.convHD = 1 / this.convertorData[0].rateSell;
+     });
     },
     methods: {
       deleteStudent(studId) {
@@ -46,6 +46,10 @@ const app = createApp({
       addStudent() {
         this.student.id = this.students.length + 1;
         this.students.push({ ...this.student });
+      },
+      convert() {
+        console.log(`conv${this.convFrom}${this.convTo}`);
+        this.convResult =this.convAmount * this[`conv${this.convFrom}${this.convTo}`]
       },
     },
   });
